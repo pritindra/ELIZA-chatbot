75cc772c2978f1970b5c6f9f071e9353110e5531
Pritindra <pritindra.d@gmail.com>
2021-05-30 00:36:58 +0530

    minor changes

--- app.py
+++ app.py
@@ -22,52 +22,52 @@ def bot():
     msg = resp.message()
     responded = False
 
-    if 'hi' in incoming_msg or 'hey' in incoming_msg or 'covid' in incoming_msg or 'menu' in incoming_msg:
+    if 'Hi' in incoming_msg or 'Hey' in incoming_msg or 'covid' in incoming_msg or 'menu' in incoming_msg:
         text = f'Hello Fella!, \nThis is ELIZA, a covid bot to provide latest information updates on corona virus for you and your family to stay stay safe.\n For any emergency 👇 \n 📞 Helpline: 011-23978046 | Toll-Free Number: 1075 \n ✉ Email: ncov2019@gov.in \n\n Please enter one of the following option 👇 \n *A*. Get information on countries and COntinents. \n *B*. Get information on vaccination in India. \n *C*. How does it *Spread*? \n *D*. *Preventive measures* to be taken. \n *E*. Latest News on Covid-19.'
         msg.body(text)
         responded = True
 
     # IF-ELSE Statements
-    if 'a' in incoming_msg:
+    if 'A' in incoming_msg:
         text = f'Please enter one of the following option: \n *1*. Get updates on all continents.\n *2*. Get updates on specific continent. \n*3*. Get updates on specific country. \n*4*. Get updates of historical data of a country.'
         msg.body(text)
         
         responded = True
 
-        if '1' in incoming_msg:
-            txt = All_Continents()
-            msg.body(txt)
-            responded = True
+    if '1' in incoming_msg:
+        txt = All_Continents()
+        msg.body(txt)
+        responded = True
 
-        if '2' in incoming_msg:
-            message = f'Please enter the continent:'
-            msg.body(message)
-            
-            txt = Specific_Continent(incoming_msg)
-            msg.body(txt)
-            responded = True
+    if '2' in incoming_msg:
+        message = f'Please enter the continent:'
+        msg.body(message)
         
-        if '3' in incoming_msg:
-            message = f'Please enter the country:'
-            msg.body(message)
-            
-            txt = Specific_Country(incoming_msg)
-            msg.body(txt)
-            responded = True
+        txt = Specific_Continent(incoming_msg)
+        msg.body(txt)
+        responded = True
+    
+    if '3' in incoming_msg:
+        message = f'Please enter the country:'
+        msg.body(message)
+        
+        txt = Specific_Country(incoming_msg)
+        msg.body(txt)
+        responded = True
 
-        if '4' in option_msg:
-            message = f'Please enter the country:\n'
-            msg.body(message)
-            coun = request.values.get('Body', '')
-            message1 = f'Please enter the number of days for historical data:'
-            msg.body(message1)
-            days = request.values.get('Body', '')
+    if '4' in incoming_msg:
+        message = f'Please enter the country:\n'
+        msg.body(message)
+        coun = request.values.get('Body', '')
+        message1 = f'Please enter the number of days for historical data:'
+        msg.body(message1)
+        days = request.values.get('Body', '')
 
-            txt = History_Country(coun,days)
-            msg.body(txt)
-            responded = True
+        txt = History_Country(coun,days)
+        msg.body(txt)
+        responded = True
 
-    if 'b' in incoming_msg:
+    if 'B' in incoming_msg:
         text = f'Please enter one of the following option: \n*1*. Get vacination centers by district. \n*2*. Get vaccination updates by PIN.'
         msg.body(text)
         option_msg = request.values.get('Body', '')
@@ -91,19 +91,19 @@ def bot():
             responded = True
 
 
-    if 'c' in incoming_msg:
+    if 'C' in incoming_msg:
         text = f'_Coronavirus spreads from an infected person through_ 👇 \n\n ♦ Small droplets from the nose or mouth which are spread when a person coughs or sneezes \n\n ♦ Touching an object or surface with these droplets on it and then touching your mouth, nose, or eyes before washing your hands \n \n ♦ Close personal contact, such as touching or shaking hands \n Please watch the video for more information 👇 https://youtu.be/0MgNgcwcKzE \n\n 👉 Type G to check the *Preventive Measures* \n 👉 Type *A, B, C, D, E* to see other options \n 👉 Type *Menu* to view the Main Menu'
         msg.body(text)
         msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
         responded = True
     
-    if 'd' in incoming_msg:
+    if 'D' in incoming_msg:
         text = f'_Coronavirus infection can be prevented through the following means_ 👇 \n ✔️ Clean hand with soap and water or alcohol-based hand rub \n https://youtu.be/EJbjyo2xa2o \n\n ✔️ Cover nose and mouth when coughing & sneezing with a tissue or flexed elbow \n https://youtu.be/f2b_hgncFi4 \n\n ✔️ Avoid close contact & maintain 1-meter distance with anyone who is coughing or sneezin \n https://youtu.be/mYyNQZ6IdRk \n\n ✔️ Isolation of persons traveling from affected countries or places for at least 14 day \n https://www.mohfw.gov.in/AdditionalTravelAdvisory1homeisolation.pdf \n\n ✔️ Quarantine if advise \n https://www.mohfw.gov.in/Guidelinesforhomequarantine.pdf \n\n 👉 Type *A, B, C, D, E, F* to see other option \n 👉 Type *Menu* to view the Main Menu'
         msg.body(text)
         msg.media('https://user-images.githubusercontent.com/34777376/77290864-1c93d000-6d03-11ea-96fe-18298535d125.jpeg')
         responded = True
 
-    if 'e' in incoming_msg:
+    if 'E' in incoming_msg:
         text = f'Please enter one of the following option: \n*1*. Get news by Keyword. \n*2*. Get news by category.'
         msg.body(text)
         option_msg = request.values.get('Body', '')
--- covid_countries.py
+++ covid_countries.py
@@ -4,9 +4,9 @@ import json
 
 def All_Continents():
     r = requests.get('https://corona.lmao.ninja/v2/continents?yesterday=true&sort')
-    if status_code == 200:
+    if r.status_code == 200:
         data = r.json()
-        text = f'data'
+        text = f'{data}'
     else:
         text = 'Sorry, could not receive the results..'
     return text
@@ -14,7 +14,7 @@ def All_Continents():
 def Specific_Continent(cont):
     r = requests.get('https://corona.lmao.ninja/v2/continents/:{cont}?yesterday&strict')
 
-    if status_code == 200:
+    if r.status_code == 200:
         data = r.json()
         text = f"__Covid updates of {data['continent']} for yesterday__ \n\nCases: *{data['cases']}* \n\nDeaths: *{data['deaths']}* \n\nRecovered: *{data['recovered']}* \n\nActive cases: *{data['active']}* \n\nCritical cases: *{data['critical']}*"
     else:
@@ -25,7 +25,7 @@ def Specific_Continent(cont):
 def Specific_Country(coun):
     r = requests.get('https://corona.lmao.ninja/v2/countries/:{coun}?yesterday=true&strict=true&query')
 
-    if status_code == 200:
+    if r.status_code == 200:
         data = r.json()
         text = f"__Covid updates of {data['country']} for yesterday__ \n\nCases: *{data['cases']}* \n\nDeaths: *{data['deaths']}* \n\nRecovered: *{data['recovered']}* \n\nActive cases: *{data['active']}* \n\nCritical cases: *{data['critical']}* \n\nTests: *{data['tests']}*"
     else:
@@ -37,7 +37,7 @@ def Specific_Country(coun):
 def History_Country(coun,days):
     r = requests.get('https://corona.lmao.ninja/v2/historical/:{coun}?lastdays={days}')
 
-    if status_code == 200:
+    if r.status_code == 200:
         data = r.json()
 
         text = f"__Covid updates of {data['country']} for {days} days  \n\nCases: *{case['cases'] for case in data['timeline']}* \n\nDeaths: *{d['deaths'] for d in data['timeline']}* \n\nRecovered: *{recover['recovered'] for recover in data['timeline']}*"
